06d6dd2d05eecdce7925018182af7a4c9742d942
Ethan Borden <ethanmborden@gmail.com>
2020-01-09 14:43:17 -0600

    reverted unintentional change

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,7 @@ require 'hedgelog'
 
 SimpleCov.minimum_coverage 99
 
-Dir[File.expand_path(File.join(File.dirname(__FILE__), 'support', '**', '*.rb'))].sort.each { |f| require f }
+Dir[File.expand_path(File.join(File.dirname(__FILE__), 'support', '**', '*.rb'))].each { |f| require f }
 
 RSpec.configure do |config|
   config.include Matchers::Benchmark
